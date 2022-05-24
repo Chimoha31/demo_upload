c2b512c5162c691688342e96283b0bc322f58f57
Chimoha31 <mokochii1108@gmail.com>
2022-05-24 15:48:07 -0700

    Created server side

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("TEST!")
+});
+
+app.listen(PORT, () =>{
+  console.log("Teddy is running 🐶")
+})
