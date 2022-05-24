346be1b37416d8d1c36c8b0d451d63b44593a1b3
Chimoha31 <mokochii1108@gmail.com>
2022-05-24 16:12:27 -0700

    Styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,221 @@
-import React from 'react'
+import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>
-      Hi, React!
+    <div className="students_container">
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
+      <ul className="student_card">
+        <li>
+          <img
+            src="https://stat.ameba.jp/user_images/20120925/13/vegitaouji/28/09/j/o0310037112205366558.jpg"
+            alt="student"
+            style={{
+              width: "180px",
+              height: "180px",
+              borderRadius: "50%",
+              border: "1px solid #ccc",
+            }}
+          />
+        </li>
+        <div className="edit_delete">
+          <li>✍</li>
+          <li>🗑</li>
+        </div>
+        <li>
+          <h2>Chiho M</h2>
+        </li>
+        <li>
+          <p>mokochii1108@gmail.com</p>
+        </li>
+        <li>
+          <p>778-1111-3333</p>
+        </li>
+        <li>
+          <p>From Cornerstone</p>
+        </li>
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
